7f4e9557d63ea36e554d1f2dd918c7e1e97dd0c4
acs <c.vauthier@codeur.online>
2021-04-20 17:38:49 +0200

    page webdeisgner

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +8,23 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/styles/main.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Logoipsum</title>
+    <title>Coeur Web</title>
 </head>
+
 <body>
     
+    
+<div class="py-5  text-center">
+  <img class="d-block mx-auto mb-4" src="/projet-binome/assets/images/icons/logo.svg" alt="" >
+</div>
+
+<ul class="nav d-flex justify-content-center my-5">
+  <li class="nav-item menu fs-4 fw-bolder ">
+    <a class="nav-link active text-dark" aria-current="page" href="#">Menu</a>
+  </li>
+  <li class="nav-item a-propos fs-4 fw-bolder">
+    <a class="nav-link text-dark" href="#">A propos</a>
+  </li>
+
+</ul>
+
