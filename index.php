664ab6017ba1fdb72df88f44a042a232d84d4005
nathbout2 <nathbout2@gmail.com>
2021-04-21 17:00:22 +0200

    landingpage1.1

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php include 'include/header.php'; ?>
+
+<main class="container-acc">
+<div class="d-flex justify-content-center">
+<img src="assets/images/logo.svg" class="logopage" alt="..."> 
+</div>
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+        <li class="nav-item coeurweb-underline">
+          <a class="nav-link active" aria-current="page" href="#">Menu</a>
+        </li>
+        <li class="nav-item coeurweb-underline">
+          <a class="nav-link" href="pages/legal-notice.php">A propos</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+      </span>
+    </div>
+  </div>
+</nav>
+<!--endnav bar-->
+<!--startintro-->
+<div class="row pt-5">
+
+<div class="col-md-5 col-sm-12 order-md-1 order-sm-2">
+    <h1 class="Coeurwebtitle">
+    Les metiers Web, pas toujours facile de s’y retrouver! 
+    </h1>
+    <h1 class="text">
+    On va vous aidez !
+    </h1>
+    <button type="button" class="btn logoipsum-btn-purple btn-lg pb-3 pt-3">Let's go</button>
+</div>
+<div class="col-md-1 col-sm-0"></div>
+<div class="col-md-6 col-sm-12 order-md-2 order-sm-1">
+    <img src="assets/images/Lost.svg" class ="imagetext">
+</div>
+
+</div>
+<!--endintro-->
+
+<!--startarticle-->
+<div class="row pt-5">
+
+<div class="col-md-1 col-sm-0"></div>
+<div class="col-md-6 col-sm-12 order-md-1 order-sm-1">
+    <img src="assets/images/Websdesigner.svg" class ="imagetext">
+</div>
+
+<div class="col-md-5 col-sm-12 order-md-2 order-sm-2">
+    <h1 class="Coeurwebtitle">
+    Web designer, qu’est ce que c’est ?
+    </h1>
+    <p class="text-black-50 fs-4">
+    Le webdesigner est un de ces métiers qui n’existaient pas il y a 20 ans.
+    Son rôle ?
+    C’est de « dessiner » ou plutôt de concevoir le graphisme d’un site ou d’un produit sur une page web Le webdesigner 
+    (appelé également infographiste ou graphiste) est, selon la formule consacrée, « le créatif multimédia du numérique ».
+ 
+    </p>
+    <div class="col-12"><a class="nav-link" href="#"><b>Learn more</b></a></div>
+   
+</div>
+
+</div>
+
+</div>
+<div class="row pt-5">
+
+<div class="col-md-5 col-sm-12 order-md-1 order-sm-2">
+    <h1 class="Coeurwebtitle">
+    Qu’est-ce que le métier de chef de projet ?
+    </h1>
+    <p class="text-black-50 fs-4">
+    Le chef de projet a pour mission principale de prendre en main le cahier des charges d’un projet et de le mettre en œuvre en analysant les besoins, réalisant des reportings… Le travail en équipe est primordial pour ce professionnel. En effet, le directeur de projet est en relation avec les différents employés d’une entreprise (graphistes, assistants-chefs de projet, chefs de produit, développeurs web, intégrateurs…) il est amené a leur confie des missions pour mener à bien le projet.
+    </p>
+    <div class="col-12"><a class="nav-link" href="#"><b>Learn more</b></a></div>
+    
+   
+</div>
+<div class="col-md-1 col-sm-0"></div>
+<div class="col-md-6 col-sm-12 order-md-2 order-sm-1">
+    <img src="assets/images/chefdeprojet.svg" class ="imagetext">
+</div>
+
+</div>
+</div>
+<!--endarticle-->
+<!--starchoice-->
+
+<div class="container3">
+     <div class="row align-items-start">
+          <div class="col">
+             <img src="assets/images/motiondesignmini.svg" class="motiondesignmini" alt="...">
+             <h5 class="tittle1"><b>Motion designer web</b></h5>
+         </div>
+          <div class="col">
+             <img src="assets/images/graphiste1.svg" class="graphiste1" alt="...">
+             <h5 class="tittle1"><b>Graphiste, un métier créatif</b></h5>
+         </div>
+          <div class="col">
+             <img src="assets/images/pentesteurminia.svg" class="pentesteurminia" alt="...">
+             <h5 class="tittle1"><b>Le Pentesteur</b></h5>
+         </div>
+      </div>
+ </div>
+</div>
+<!--endchoice-->
+
+
+</main>
+<?php include 'include/footer.php'; ?>
