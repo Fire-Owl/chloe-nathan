65743b61f7f05b2c2f828ee218f462705d1964b0
acs <c.vauthier@codeur.online>
2021-05-05 08:52:24 +0200

    test final

--- include/header.php
+++ include/header.php
@@ -5,11 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Coeur Web est un site web informatif pour découvrir les différents métiers du numérique.">
-<<<<<<< HEAD
     <meta name="author" content="Nathand Boutaud et Chloé Vauthier">  
-=======
-    <meta name="author" content="Nathand Boutaud et Chloé Vauthier">
->>>>>>> 188b72326d2b5ef9ecb40d98218e1d8b887efa0d
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/styles/main.css">
     <link rel="shortcut icon" href="../assets/images/logonotittle.svg" type="image/svg+xml">
